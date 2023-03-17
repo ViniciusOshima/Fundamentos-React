2e904b2ca39749a86117938f36e789058287000f
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-03-17 16:46:27 -0300

    FEAT introduzindo funcionalidade ao projeto, introduziando também um banco de dados e imprimindo-os em tela

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,39 @@ import style from './App.module.css'
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/ViniciusOshima.png',
+      name: 'Vinícius Oshima',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: ' Fala galeraa 👋 '},
+      {type: 'paragraph', content: ' Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 '},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ], 
+    publishedAt: new Date('2023-02-03 20:00:00')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/RafaelOshima.png',
+      name: 'Rafael Oshima',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: ' Fala galeraa 👋 '},
+      {type: 'paragraph', content: ' lorem agua uma substancia essencial para vida humana é um bem comum para muitas pessoas mas para outras não 🚀 '},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ], 
+    publishedAt: new Date('2023-02-10 20:00:00')
+  },
+
+]
+
   function App() {
     return (
       <div>
@@ -15,13 +48,15 @@ import './global.css';
           <Sidebar />
 
           <main>
-            <Post 
-              author= 'Gabriel'
-              content= 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat velit et deleniti eveniet minus fuga. Temporibus maxime quod minima quae perferendis harum sint vero aspernatur et exercitationem? Dignissimos, non distinctio.' />
-
-            <Post
-              author= 'Jesus'
-              content= 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat velit et deleniti eveniet minus fuga. Temporibus maxime quod minima quae perferendis harum sint vero aspernatur et exercitationem? Dignissimos, non distinctio.' />
+            {posts.map(post => {
+              return (
+                <Post
+                  author= {post.author}
+                  content= {post.content}
+                  publishedAt= {post.publishedAt} 
+                />
+              )
+            })}
           </main>
         </div>
       </div>
